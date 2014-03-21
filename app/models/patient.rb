ffa2c57790b6099a0202069191c3a462ff8ba066
David Simons <dave.c.simons@gmail.com>
2014-03-21 15:33:09 -0700

    added confirmable, lockable and timeoutable modules and removed rememberable module from patient.rb model file

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,6 +1,7 @@
 class Patient < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :rememberable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :trackable, :validatable,
+         :confirmable, :lockable, :timeoutable
 end
