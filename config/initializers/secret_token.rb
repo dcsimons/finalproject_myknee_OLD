690b406d96d00bea9af351501b683b488d7855d3
David Simons <dave.c.simons@gmail.com>
2014-03-21 14:49:12 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyKnee::Application.config.secret_key_base = 'a1e4fcc83ba6a966ca8594666a31ea5806a8fc17bd89e206ae5a3de001e9475e185631efa719cb736aa402b4f83202dc0350bdabc885940d701d3a2a771fee25'
